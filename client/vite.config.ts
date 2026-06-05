c6720648e8ae00f444be428210782516d16d0451
Munkh-Orgil <munhorgilmunhtsetseg333@gmail.com>
2026-06-05 12:43:13 +0800

    Delete client directory
    fail deisgn

--- client/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteCompression from 'vite-plugin-compression';
-export default defineConfig({
-  plugins: [react(), viteCompression()]
-});
